5c1eb525fbc071468c82d7212256d76dd8d53919
Josh Maddigan <josh.maddigan@gmail.com>
2026-06-26 16:58:31 -0400

    railway debugging

--- app.py
+++ app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def index():
     return send_from_directory("static", "Flappy_Spikes.html")
 
+@app.after_request
+def add_headers(response):
+    response.headers["Cross-Origin-Opener-Policy"] = "same-origin"
+    response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
+    return response
+
+@app.route("/<path:filename>")
+def serve_static(filename):
+    return send_from_directory("static", filename)
+
+
+
+
 if __name__ == "__main__":
     app.run()
