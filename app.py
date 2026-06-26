df6fb08289853a9d487ead117ab07574d23bbc90
Josh Maddigan <josh.maddigan@gmail.com>
2026-06-26 16:54:10 -0400

    add procfile

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return send_from_directory("static", "index.html")
+    return send_from_directory("static", "Flappy_Spikes.html")
 
 if __name__ == "__main__":
     app.run()
